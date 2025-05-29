3f0d6554294d7d56e4702aa91909edf426a63e07
solhope23 <s526413103@gmail.com>
2025-05-29 11:30:27 +0300

    FirstCommit

---
+++ IDF_Oporation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDF_oporation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
